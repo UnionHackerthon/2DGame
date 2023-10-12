b9a1c151a5d341b9fbf43329542c54fa93e7ed53
Balloon208 <gnswldms2@gmail.com>
2023-10-12 12:49:35 +0900

    ADD : Mob Move

--- 2DGame/Assets/Script/Monster.cs
+++ 2DGame/Assets/Script/Monster.cs
@@ -2,7 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Monster : MonoBehaviour
+public class Monster : MobStatus
 {
+    public Transform player;
 
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+    void Update()
+    {
+        transform.LookAt(player);
+
+        transform.position = Vector3.MoveTowards(transform.position, player.position, movespeed * Time.deltaTime);
+        
+    }
 }
