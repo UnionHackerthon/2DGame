3284d118e3d388772d1d30d0d758bed9d0fbd284
Balloon208 <gnswldms2@gmail.com>
2023-10-12 19:47:48 +0900

    Add : BackEnd

---
+++ 2DGame/Assets/Script/BackEnd/SpawnManagerScriptableObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class SpawnManagerScriptableObject : ScriptableObject
+{
+    public string id;
+    public int score;
+}
